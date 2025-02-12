45b78b15e828e5ab6dc01ff3957b7cc1931bc7db
isaacknk <156215772+isaacknk@users.noreply.github.com>
2025-02-11 21:31:20 -0300

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+GREEN='\033[1;32m'
+while : 
+do
+echo "${GREEN} INICIANDO BASE API..."
+    node index.js
+    sleep 1
+
+done
+
+
+#sistema para o bot não cair
+# de sh start.sh no termux!!
